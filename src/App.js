8b64ef8e1558bbe9c87ba22a41f2526514500801
MinYeong Park <judice0465@gmail.com>
2025-01-07 23:43:49 +0900

    useEffect 추가

--- src/App.js
+++ src/App.js
@@ -8,15 +8,16 @@ function App() {
   const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
-  console.log("I run all the time");
+ 
   useEffect(() => {
-    console.log("CALL THE API...");
+    console.log("I run only once.");
   }, []);
   useEffect(() => {
-    if(keyword != "" && keyword.length > 5){
-      console.log("SEARCH FOR" , keyword);
-    }
+    console.log("I run when 'keyword' changes...");
   }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes...");
+  }, [counter]);
 
   return (
     <div>
