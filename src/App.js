332f2a823404a62a242e63b05de679aa9e39f29e
MinYeong Park <judice0465@gmail.com>
2025-01-07 23:30:56 +0900

    useState keyword 추가

--- src/App.js
+++ src/App.js
@@ -9,16 +9,20 @@ function App() {
   const onClick = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
   console.log("I run all the time");
- const iRunOnlyOnce = () => {
-  console.log("i run only once");
- };
- useEffect(() => {
-  console.log("CALL THE API...");
- }, []);
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, []);
+  useEffect(() => {
+    console.log("SEARCH FOR" , keyword);
+  }, []);
 
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."/>
+      <input 
+              value={keyword} 
+              onChange={onChange} 
+              type="text" 
+              placeholder="Search here..."/>
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
