ac7187b537970dd9870b0c5f5d60f172caf60f0b
MinYeong Park <judice0465@gmail.com>
2025-01-07 23:59:11 +0900

    Hello 메서드 추가

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import PropTypes from "prop-types";
 import styles from "./App.module.css";
 import {useEffect, useState} from "react";
 
+function Hello(){
+  return <h1>Hello</h1>
+}
+
 function App() {
   const [showing, setShowing] = useState(false);
   const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
+      {}
       <button onClick={onClick}>{showing ? "HIDE" : "Show"}</button>
     </div>
   );
