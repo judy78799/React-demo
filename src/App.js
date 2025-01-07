8edfe42d069c75cef22d85e19cc2fa11274f7a7d
MinYeong Park <judice0465@gmail.com>
2025-01-07 23:07:09 +0900

    useEffect 추가

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import Button from "./Button";
 import PropTypes from "prop-types";
 import styles from "./App.module.css";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => 
-    console.log('render');
     setValue((prev) => prev + 1);
-    
+  console.log("I run all thee time");
+ const iRunOnlyOnce = () => {
+  console.log("i run only once");
+ };
+ useEffect(() => {
+  console.log("CALL THE API...");
+ }, []);
+
   return (
     <div>
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
-      <Button text={"Continue"}/>
     </div>
   );
 }
