8b46b613b242c48c101fb09b96959b1471c0cbd8
MinYeong Park <judice0465@gmail.com>
2025-01-08 14:40:51 +0900

    To Do 배열 만들기

--- src/App.js
+++ src/App.js
@@ -22,20 +22,21 @@ const onSubmit = (event) => {
   if(toDo === ""){
     return;
   }
+  setToDos((currentArray) => [toDo, ...currentArray]); //여기에 직전의 값을 받아올 것임.
   setTodo("");
-  setToDos(); //여기에 직전의 값을 받아올 것임.
 };
 console.log(toDos);
   return (
     <div>
+      <h1>My To Dos ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
         <input 
           onChange={onChange} 
           value={toDo} 
           type="text" 
           placeholder="Write your to do,,,"/>
-      </form>
       <button>Add To Do</button>
+      </form>
     </div>
   );
 }
